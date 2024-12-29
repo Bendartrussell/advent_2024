7d70c58e40084b7f57821e02012fba5092e36b35
Benjamin <bendartrussell@gmail.com>
2024-12-28 23:49:49 -0500

    day 2 part 2

--- src/advent_2024/day2/day2_core.py
+++ src/advent_2024/day2/day2_core.py
@@ -8,8 +8,12 @@ def part1(data: str) -> int:
     return safe_count
 
 
-def part2():
-    pass
+def part2(data: str) -> int:
+    reports = parse_reports(data)
+    results = list(is_safe(report, dampener=True) for report in reports)
+    safe_count = sum(results)
+    return safe_count
+
 
 
 def parse_reports(data: str) -> list[Report]:
@@ -18,12 +22,24 @@ def parse_reports(data: str) -> list[Report]:
     ]
 
 
-def is_safe(report: Report) -> bool:
+def is_safe(report: Report, *, dampener: bool = False) -> bool:
     diffs = [two - one for one, two in zip(
         report[:-1],
         report[1:],
     )]
     if diffs[0] >= 0:
-        return all(1 <= d <= 3 for d in diffs)
+        level_safety = [1 <= d <= 3 for d in diffs]
     else:
-        return all(-3 <= d <= -1 for d in diffs)
+        level_safety = [-1 >= d >= -3 for d in diffs]
+    
+    safety = all(level_safety)
+    if safety or not dampener:
+        return safety
+    
+    for i in range(len(report)):
+        new_report = report.copy()
+        new_report.pop(i)
+        if is_safe(new_report):
+            return True
+    else:
+        return False
--- test/unit/day2/test_day2.py
+++ test/unit/day2/test_day2.py
@@ -19,3 +19,21 @@ def test_part1():
     in_path = Path().cwd() / "test/unit/day2/data/input.txt"
     with in_path.open() as in_file:
         assert part1(in_file.read()) == 326
+
+
+def test_part2_example():
+    example_data = (
+        "7 6 4 2 1\n"
+        "1 2 7 8 9\n"
+        "9 7 6 2 1\n"
+        "1 3 2 4 5\n"
+        "8 6 4 4 1\n"
+        "1 3 6 7 9\n"
+    )
+    assert part2(example_data) == 4
+
+
+def test_part2():
+    in_path = Path().cwd() / "test/unit/day2/data/input.txt"
+    with in_path.open() as in_file:
+        assert part2(in_file.read()) == 381
